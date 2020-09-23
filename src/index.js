d7d107f38ffc714b9da446aec64936505bb4a040
Manuel Barajas <manuel.jose.barajas@gmail.com>
2020-09-23 08:29:43 -0400

    adding history and router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router }  from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history = {history}><App history = {history} /></Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
