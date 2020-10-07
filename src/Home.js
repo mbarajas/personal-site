cf3ad287d6c11dd60eb5d33ead42252318d7a5b7
Manuel Barajas <manuel.jose.barajas@gmail.com>
2020-10-07 19:49:54 -0400

    working on styling and adding bio

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './Home.css';
 
 function Home() {
   return(
-    <div className="Home">
-      <header className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
-        <h1>
-          Manuel Jose Barajas
-        </h1>
-        <div className="columns is-mobile">
-          <div className="column">
-            <a className="Home-link" href="https://github.com/mbarajas" target="_blank" rel="noopener noreferrer">
-              GitHub
-            </a>
-          </div>
-          <div className="column">
-            <a className="Home-link" href="https://www.linkedin.com/in/manuel-barajas" target="_blank" rel="noopener noreferrer">
-              LinkedIn
-            </a>
-          </div>
+    <div className="Home content">
+      <div className="section">
+        <div className="container">
+          <h1>
+            Manuel Jose Barajas
+          </h1>
+          <p>
+            Personal Website and Sandbox
+          </p>
+            <div className="column">
+              <a className="Home-link" href="https://github.com/mbarajas" target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>
+            </div>
+            <div className="column">
+              <a className="Home-link" href="https://www.linkedin.com/in/manuel-barajas" target="_blank" rel="noopener noreferrer">
+                LinkedIn
+              </a>
+            </div>
         </div>
-      </header>
-      <p> This is my website </p>
+      </div>
+      <div className="section">
+        <div className="container">
+          <h1> About Me </h1>
+          <p> My name is Manuel Jose Barajas. I'm a Software Engineer specializing in System Architecture and DevOps. I'm currently based in Atlanta, Georgia.</p>
+        </div>
+      </div>
     </div>
   );
 
